c558cbce987da5fd8f6b3034ae11c5b8a7e1f408
Malma <itsmalma28@gmail.com>
2023-03-03 19:45:11 +0700

    now run migration need *sql.DB

--- runner.go
+++ runner.go
@@ -1,6 +1,9 @@
 package gomimi
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+)
 
 type Runner struct {
 	indicator Indicator
@@ -11,7 +14,7 @@ func NewRunner(indicator Indicator, builder Builder) Runner {
 	return Runner{indicator, builder}
 }
 
-func (runner Runner) RunMigration(migrations ...Migration) {
+func (runner Runner) RunMigration(db *sql.DB, migrations ...Migration) {
 	currentMigrationName := runner.indicator.Current()
 	notFound := true
 	if currentMigrationName == "" {
@@ -20,16 +23,56 @@ func (runner Runner) RunMigration(migrations ...Migration) {
 
 	for _, migration := range migrations {
 		if !notFound {
+			// begin (for up migration)
 			runner.builder.Begin()
+			// run up migration
 			if err := migration.Up(runner.builder); err != nil {
+				// if up fail
+				// do the rollback
 				runner.builder.Rollback()
+				// begin (for down migration)
 				runner.builder.Begin()
+				// run down migration
 				if err := migration.Down(runner.builder); err != nil {
+					// if down fail
+					// do the rollback
 					runner.builder.Rollback()
+					// and panic the error
+					panic(err)
+				} else if _, err := db.Exec(runner.builder.Commit()); err != nil {
+					// if down success
+					// do commit and run the query
+					// but if query fail
+					// do rollback
+					runner.builder.Rollback()
+					// and also panic the error
+					panic(err)
+				}
+			} else if _, err := db.Exec(runner.builder.Commit()); err != nil {
+				// if up success
+				// do commit and run the query
+				// but if query fail
+				// do rollback
+				runner.builder.Rollback()
+				// begin (for down migration)
+				runner.builder.Begin()
+				// run down migration
+				if err := migration.Down(runner.builder); err != nil {
+					// if down fail
+					// do the rollback
+					runner.builder.Rollback()
+					// and panic the error
+					panic(err)
+				} else if _, err := db.Exec(runner.builder.Commit()); err != nil {
+					// if down success
+					// do commit and run the query
+					// but if query fail
+					// do rollback
+					runner.builder.Rollback()
+					// and also panic the error
 					panic(err)
 				}
 			}
-			runner.builder.Commit()
 			currentMigrationName = migration.Name()
 		} else if migration.Name() == currentMigrationName {
 			notFound = false
